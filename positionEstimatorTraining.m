da46bec4c51bd870d53c768db71e8be318d61274
vrn22 <126903704+vrn22@users.noreply.github.com>
2023-03-20 13:31:39 +0000

    Add files via upload

--- positionEstimatorTraining.m
+++ positionEstimatorTraining.m
@@ -74,9 +74,37 @@ function [modelParameters] = positionEstimatorTraining(training_data)
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%% LINEAR REGRESSION %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-% Gaussian kernel full set of spikes
+% Gaussian kernel full set of spikes. Binned every 20 secs
 
-[train_full] = gaussian(training_data,20.0);
+    train_new_bins2 = struct;
+    
+    new_bin2 = 20; % ms
+    
+    for i = 1:nangles
+        for j = 1:trials_per_angle
+
+            all_spikes = training_data(j,i).spikes; % spikes is no neurons x no time points
+            nneurons = size(all_spikes,1);
+            t_length = size(all_spikes,2);
+            t_new = 1: new_bin2 : t_length; % New time point array
+            handPos = training_data(j,i).handPos(1:2,:);
+            spikes = zeros(nneurons,numel(t_new)-1);
+
+            for k = 1 : numel(t_new) - 1 % get rid of the paddded bin
+                spikes(:,k) = sum(all_spikes(:,t_new(k):t_new(k+1)-1),2);
+            end
+
+            spikes = sqrt(spikes);
+            binned_handPos = handPos(:,t_new);
+            
+            train_new_bins2(j,i).spikes = spikes;
+            train_new_bins2(j,i).handPos = binned_handPos;
+        end
+    end
+
+
+
+[train_full] = gaussian(train_new_bins2,new_bin2);
 
     n_PCA_components = 25;
     for reaching_angle = 1:nangles
